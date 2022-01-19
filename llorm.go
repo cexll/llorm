2a761dbae2a4b5b05d78d2ce76b0a4c716637fff
cexll <evanxian9@gmail.com>
2022-01-19 16:04:08 +0800

    feat: migrate

--- llorm.go
+++ llorm.go
@@ -2,6 +2,8 @@ package llorm
 
 import (
 	"database/sql"
+	"fmt"
+	"strings"
 
 	"github.com/go-ll/llorm/dialect"
 	"github.com/go-ll/llorm/log"
@@ -11,27 +13,6 @@ import (
 type Engine struct {
 	db      *sql.DB
 	dialect dialect.Dialect
-}
-
-type TxFunc func(*session.Session) (interface{}, error)
-
-func (e *Engine) Transaction(f TxFunc) (result interface{}, err error) {
-	s := e.NewSession()
-	if err := s.Begin(); err != nil {
-		return nil, err
-	}
-	defer func() {
-		if p := recover(); p != nil {
-			_ = s.Rollback()
-			panic(p) // re-throw panic after Rollback
-		} else if err != nil {
-			_ = s.Rollback() // err is non-nil; don't change it
-		} else {
-			err = s.Commit() // err is nil; if Commit returns error update err
-		}
-	}()
-
-	return f(s)
 }
 
 func NewEngine(driver, source string) (e *Engine, err error) {
@@ -66,4 +47,74 @@ func (e *Engine) Close() {
 
 func (e *Engine) NewSession() *session.Session {
 	return session.New(e.db, e.dialect)
+}
+
+type TxFunc func(*session.Session) (interface{}, error)
+
+func (e *Engine) Transaction(f TxFunc) (result interface{}, err error) {
+	s := e.NewSession()
+	if err := s.Begin(); err != nil {
+		return nil, err
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			_ = s.Rollback()
+			panic(p) // re-throw panic after Rollback
+		} else if err != nil {
+			_ = s.Rollback() // err is non-nil; don't change it
+		} else {
+			err = s.Commit() // err is nil; if Commit returns error update err
+		}
+	}()
+
+	return f(s)
+}
+
+// difference returns a - b
+func difference(a []string, b []string) (diff []string) {
+	mapB := make(map[string]bool)
+	for _, v := range b {
+		mapB[v] = true
+	}
+	for _, v := range a {
+		if _, ok := mapB[v]; !ok {
+			diff = append(diff, v)
+		}
+	}
+	return
+}
+
+func (e *Engine) Migrate(value interface{}) error {
+	_, err := e.Transaction(func(s *session.Session) (result interface{}, err error) {
+		if !s.Model(value).HasTable() {
+			log.Info("table %s doesn't exist", s.RefTable().Name)
+			return nil, s.CreateTable()
+		}
+		table := s.RefTable()
+		rows, _ := s.Raw(fmt.Sprintf("SELECT * FROM %s LIMIT 1", table.Name)).QueryRows()
+		columns, _ := rows.Columns()
+		addCols := difference(table.FieldNames, columns)
+		delCols := difference(columns, table.FieldNames)
+		log.Info("added cols %v, deleted cols %v", addCols, delCols)
+
+		for _, col := range addCols {
+			f := table.GetField(col)
+			sqlStr := fmt.Sprintf("ALTER TABLE %s ADD COLUMN %s %s;", table.Name, f.Name, f.Type)
+			if _, err = s.Raw(sqlStr).Exec(); err != nil {
+				return
+			}
+		}
+
+		if len(delCols) == 0 {
+			return
+		}
+		tmp := "tmp_" + table.Name
+		fieldStr := strings.Join(table.FieldNames, ", ")
+		s.Raw(fmt.Sprintf("CREATE TABLE %s SELECT %s FROM %s;", tmp, fieldStr, table.Name))
+		s.Raw(fmt.Sprintf("DROP TABLE %s;", table.Name))
+		s.Raw(fmt.Sprintf("ALERT TABLE %s RENAME TO %s;", tmp, table.Name))
+		_, err = s.Exec()
+		return
+	})
+	return err
 }
